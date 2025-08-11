59d4c19caf29c2e10724180aa8175fefd06ca0c4
Vince <vincentmichaelpalma@gmail.com>
2025-08-10 19:51:31 -0700

    (feat) added google auth with supabase allowing users to log in/out with google account

--- frontend/ta-app/src/App.jsx
+++ frontend/ta-app/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { supabase } from '../utils/supabase'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() =>{
+    // load session
+
+    // sees if there is a session, returns a promise then
+    // we use then to handle the promise and we data as a res data
+    // then we set the user to its session user or null if not
+    supabase.auth.getSession().then(({data}) => {
+      setUser(data.session?.user ?? null)
+    });
+
+    // listen for changes and store data variable returned in variable called sub
+    // using supabase auth api to use onAuthStateChange event listener
+    // this will be triggered everytime a sign in or sing out occurs
+    // when an event happens supabase will send the type of even (either sign in/out as the _event)
+    // and current session object after the change
+    // then set the new session to the user
+    // and give subscriptio data of session to sub so we can unsub later to the event listener
+    const {data:sub} = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    // will run when component is unmounted and we unsub from the even listner ^
+    return () => sub.subscription.unsubscribe();
+  }, []);
+
+
+    // calls supbase login with google
+    // it redirect user to google login pop up
+    const signInWithGoogle = () => {
+    supabase.auth.signInWithOAuth({ provider: "google" });
+  };
+
+  // tells supabase to sign out
+    const signOut = () => {
+    supabase.auth.signOut();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: 24 }}>
+      {user ? (
+        <>
+          <div>Signed in as <b>{user.email}</b></div>
+          <button onClick={signOut}>Sign out</button>
+        </>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
+    </div>
+  );
 }
-
-export default App
