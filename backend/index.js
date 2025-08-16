73afd5c84bcf1b41e9df25c6c24fd6d940e95737
Vince <vincentmichaelpalma@gmail.com>
2025-08-15 22:02:29 -0700

    (feat) connected frontend to back with auth route with  middleware to veirfy jwt token from supabase

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,25 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
+
+// middlewear functiuon -- will move to its own file
+async function requireUser(req, res, next){ //req is the incoming requestm, res will be response object, and next is fo the middlewear
+  const auth = req.headers.authorization || ""; // getting auth jwt token from request, if doesnt exist put an empty string
+  const token = auth.startsWith("Bearer ") ? auth.slice(7) : null; // pull out token from Bearer type
+  if (!token) return res.status(401).json({ error: "Missing token" }); // if no token return from middlewear with 401
+
+  const { data, error } = await supabase.auth.getUser(token); // use supabase's api to get repsonse object with either the user or error
+  if (error || !data?.user) return res.status(401).json({ error: "Invalid token" }); // if error or no user token is invalid
+
+  req.user = data.user; // put user data in request object so we can use it in the route
+  next();
+}
+
+app.get("/api/me", requireUser, (req, res) => {
+  res.json({ id: req.user.id, email: req.user.email });
+});
+
+
 // Health check route
 app.get("/api/health", async (req, res) => {
   try {
--- frontend/ta-app/src/App.jsx
+++ frontend/ta-app/src/App.jsx
@@ -42,16 +42,37 @@ export default function App() {
     supabase.auth.signOut();
   };
 
+  async function fetchMe() {
+    const { data } = await supabase.auth.getSession(); // supabase storing session object in local storage
+    const token = data.session?.access_token; // getting token
+    if (!token) return alert("No session token"); // if token does not exist return
+
+    // use backend api to get user info and send token for middleware
+    const res = await fetch("http://localhost:4000/api/me", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    // person object
+    const me = await res.json();
+    console.log("me:", me);
+    alert(`Backend says you are: ${me.email || JSON.stringify(me)}`);
+  }
+
   return (
     <div style={{ padding: 24 }}>
-      {user ? (
-        <>
-          <div>Signed in as <b>{user.email}</b></div>
-          <button onClick={signOut}>Sign out</button>
-        </>
-      ) : (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-      )}
+          {user ? (
+      <>
+        <div>Welcome, <b>{user.email}</b></div>
+        <button onClick={signOut}>Sign out</button>
+        <button onClick={fetchMe}>Test /api/me</button>
+      </>
+    ) : (
+      <>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <br></br>
+      <text>Test</text>
+      </>
+    )}
     </div>
   );
 }
